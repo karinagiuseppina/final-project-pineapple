89bce658f43f43f5c9c423e298d50665fc62334d
karinagiuseppina <karina.giuseppina@gmail.com>
2021-09-29 19:01:49 +0000

    JWT Auth config

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -11,6 +12,15 @@ api = Blueprint('api', __name__)
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
     response_body = {
         "message": "Hello! I'm a message that came from the backend"
     }
