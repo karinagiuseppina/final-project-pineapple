6f0c989915088ae1f106c167e84c5095548aca55
Fernando Salamanca <lfernandosalamanca@gmail.com>
2021-10-04 19:18:04 +0000

    Implemented centros table and test database JSON

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,12 +1,30 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import json
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pareja, Tiempo_proceso, Centro, Tratamiento
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
+
+@api.route("/testdb", methods=['GET'])
+def fill_database():
+    f = open("/workspace/final-project-pineapple/src/api/testDatabase.JSON", "r")
+    content = f.read()
+    jsondecoded = json.loads(content)
+
+    for centro in jsondecoded['centros']:
+        new_centro = Centro(tipo = centro['tipo'], peso = centro['peso'])
+        db.session.add(new_centro)
+    
+    db.session.commit()
+    centros = Centro.query.all()
+    centros = list(map (lambda centro: centro.serialize(), centros))   
+
+    return jsonify({"msg": "OK!"})
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
