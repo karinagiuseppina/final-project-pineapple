7a8a9517ce7d3318f5646dfb75b12ae2e0cb21f8
karinagiuseppina <karina.giuseppina@gmail.com>
2021-10-06 18:04:59 +0000

    Precarga de datos y modelo actualizado.

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Pareja, Tratamiento, Tiempo_proceso, Centro
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Pareja, db.session))
+    admin.add_view(ModelView(Tratamiento, db.session))
+    admin.add_view(ModelView(Tiempo_proceso, db.session))
+    admin.add_view(ModelView(Centro, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pareja, Tratamiento, Tiempo_proceso, Centro
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
+import json
+import bcrypt
 
 api = Blueprint('api', __name__)
 
@@ -17,10 +19,27 @@ def fill_database():
     for centro in jsondecoded['centros']:
         new_centro = Centro(tipo = centro['tipo'], peso = centro['peso'])
         db.session.add(new_centro)
+
+    for tratamiento in jsondecoded['tratamientos']:
+        new_tratamiento = Tratamiento(tipo = tratamiento['tipo'], peso = tratamiento['peso'])
+        db.session.add(new_tratamiento)
+
+    for rango_tiempo_proceso in jsondecoded['tiempo_proceso']:
+        new_tiempo = Tiempo_proceso(menor_valor = rango_tiempo_proceso['menor_valor'], mayor_valor = rango_tiempo_proceso['mayor_valor'], peso = rango_tiempo_proceso['peso'])
+        db.session.add(new_tiempo)
+
+    for pareja in jsondecoded['pareja']:
+        new_pareja = Pareja(opcion = pareja['opcion'], peso = pareja['peso'])
+        db.session.add(new_pareja)
     
+    for user in jsondecoded['users']:
+        password = user['password'].encode('utf8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        new_user = User(name = user['name'], email = user['email'], password = hashed_password, edad = user['edad'], num_aborto = user['num_aborto'])
+        db.session.add(new_user)
+
     db.session.commit()
-    centros = Centro.query.all()
-    centros = list(map (lambda centro: centro.serialize(), centros))   
 
     return jsonify({"msg": "OK!"})
 
@@ -42,3 +61,20 @@ def protected():
     }
 
     return jsonify(response_body), 200
+
+# @api.route("/findpossiblematches", methods=["GET"])
+# @jwt_required()
+# def find_possible_matches():
+
+#     actual_user: 
+
+#     result = session.query(User).filter(User.edad + 5 > , User.edad <, Customers.name.like('Ra%'))
+
+#     for row in result:
+#         print ("ID:", row.id, "Name: ",row.name, "Address:",row.address, "Email:",row.email)
+
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend"
+#     }
+
+#     return jsonify(response_body), 200
