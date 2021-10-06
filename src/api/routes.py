7666cf8d30e8b788f6d3646cd9ed021ed46be74c
Fernando Salamanca <lfernandosalamanca@gmail.com>
2021-10-06 19:20:44 +0000

    fixed DB errors

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,68 +1,47 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import json
 from flask import Flask, request, jsonify, url_for, Blueprint
-#from api.models import db, User, Pareja, Tiempo_proceso, Centro, Tratamiento
+from api.models import db, User, Pareja, Tratamiento, Tiempo_proceso, Centro
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
+import json
+import bcrypt
 
 api = Blueprint('api', __name__)
 
-#### FILLS TEST DATABASE
 @api.route("/testdb", methods=['GET'])
 def fill_database():
     f = open("/workspace/final-project-pineapple/src/api/testDatabase.JSON", "r")
     content = f.read()
     jsondecoded = json.loads(content)
 
-#### USERS
-    for user in jsondecoded['users']:
-        new_user = User(name = user['name'], email = user['email'], edad = user['edad'], num_aborto = user['num_aborto'], password = user['password'])
-        db.session.add(new_user)
-    
-    db.session.commit()
-    users = User.query.all()
-    users = list(map (lambda user: user.serialize(), users)) 
-
-#### CENTROS
     for centro in jsondecoded['centros']:
         new_centro = Centro(tipo = centro['tipo'], peso = centro['peso'])
         db.session.add(new_centro)
-    
-    db.session.commit()
-    centros = Centro.query.all()
-    centros = list(map (lambda centro: centro.serialize(), centros)) 
 
-#### TRATAMIENTOS
-    for tratamiento in jsondecoded['centros']:
+    for tratamiento in jsondecoded['tratamientos']:
         new_tratamiento = Tratamiento(tipo = tratamiento['tipo'], peso = tratamiento['peso'])
         db.session.add(new_tratamiento)
-    
-    db.session.commit()
-    tratamientos = Tratamiento.query.all()
-    tratamientos = list(map (lambda tratamiento: tratamiento.serialize(), tratamientos)) 
 
-#### TIEMPO PROCESO
-    for tiempo in jsondecoded['tiempo_proceso']:
-        new_tiempo = Tiempo_proceso(menor_valor = tiempo['menor_valor'], mayor_valor = tiempo['mayor_valor'], peso = tiempo['peso'])
+    for rango_tiempo_proceso in jsondecoded['tiempo_proceso']:
+        new_tiempo = Tiempo_proceso(menor_valor = rango_tiempo_proceso['menor_valor'], mayor_valor = rango_tiempo_proceso['mayor_valor'], peso = rango_tiempo_proceso['peso'])
         db.session.add(new_tiempo)
-    
-    db.session.commit()
-    tiempos = Tiempo_proceso.query.all()
-    tiempos = list(map (lambda tiempo: tiempo.serialize(), tiempos))
 
-#### PAREJAS
     for pareja in jsondecoded['pareja']:
         new_pareja = Pareja(opcion = pareja['opcion'], peso = pareja['peso'])
         db.session.add(new_pareja)
     
+    for user in jsondecoded['users']:
+        password = user['password'].encode('utf8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        new_user = User(name = user['name'], email = user['email'], password = hashed_password, edad = user['edad'], num_aborto = user['num_aborto'])
+        db.session.add(new_user)
+
     db.session.commit()
-    parejas = Pareja.query.all()
-    parejas = list(map (lambda pareja: pareja.serialize(), parejas))   
 
-    return jsonify({"msg": "OK!"}), 200
-
+    return jsonify({"msg": "OK!"})
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -74,6 +53,31 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+# @api.route("/findpossiblematches", methods=["GET"])
+# @jwt_required()
+# def find_possible_matches():
+
+#     actual_user: 
+
+#     result = session.query(User).filter(User.edad + 5 > , User.edad <, Customers.name.like('Ra%'))
+
+#     for row in result:
+#         print ("ID:", row.id, "Name: ",row.name, "Address:",row.address, "Email:",row.email)
+
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend"
+#     }
+
+#     return jsonify(response_body), 200
 
 #### LOGIN
 @api.route('/login', methods=['POST'])
