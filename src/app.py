bc853031aef760d96a470fc65a3903fb8b9ab9e7
Fernando Salamanca <lfernandosalamanca@gmail.com>
2021-10-11 21:54:38 +0000

    login finished, logout button created

--- src/app.py
+++ src/app.py
@@ -2,37 +2,24 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from datetime import timedelta
-import redis
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db, User, Couple, Treatment, Process, Center
+from api.models import db
 from api.routes import api
 from api.admin import setup_admin
-from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt, get_jwt_identity, set_access_cookies, unset_jwt_cookies
+from flask_jwt_extended import JWTManager 
 import json
-import bcrypt
-
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# Time delta for JWTs to expire
-ACCESS_EXPIRES = timedelta(hours=1)
-
 app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
 jwt = JWTManager(app)
-
-# Setup redis connection for storing the blocklisted tokens.
-jwt_redis_blocklist = redis.StrictRedis(
-    host="0.0.0.0", port=3001, db=0, decode_responses=True
-)
 
 # database condiguration
 if os.getenv("DATABASE_URL") is not None:
@@ -73,78 +60,6 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
-
-# TEST DB    
-@app.route("/testdb", methods=['GET'])
-def fill_database():
-    f = open("/workspace/final-project-pineapple/src/api/testDatabase.JSON", "r")
-    content = f.read()
-    jsondecoded = json.loads(content)
-
-    for center in jsondecoded['centers']:
-        new_center = Center(type = center['type'], weight = center['weight'])
-        db.session.add(new_center)
-
-
-    for treatment in jsondecoded['treatments']:
-        new_treatment = Treatment(type = treatment['type'], weight = treatment['weight'])
-        db.session.add(new_treatment)
-
-    for time_slot in jsondecoded['process_time_slots']:
-        new_time = Process(min_value = time_slot['min_value'], max_value = time_slot['max_value'], weight = time_slot['weight'])
-        db.session.add(new_time)
-
-    for couple in jsondecoded['couples']:
-        new_couple = Couple(option = couple['option'], weight = couple['weight'])
-        db.session.add(new_couple)
-    
-    for user in jsondecoded['users']:
-        password = user['password'].encode('utf8')
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password, salt)
-        decoded_password = hashed_password.decode('utf8')
-        new_user = User(name = user['name'], email = user['email'], password = decoded_password, age = user['age'], abortion_num = user['abortion_num'])
-        db.session.add(new_user)
-
-    db.session.commit()
-
-    return jsonify({"msg": "OK!"})
-
-# Callback function to check if a JWT exists in the redis blocklist. jti = JWT unique identifier
-@jwt.token_in_blocklist_loader
-def check_if_token_is_revoked(jwt_header, jwt_payload):
-    print(jwt_header, jwt_payload)
-    token_unique_id = jwt_payload["jti"]
-    print(token_unique_id)
-    token_in_redis = jwt_redis_blocklist.get(token_unique_id)
-    print(token_in_redis)
-    return token_in_redis is not None
-
-#### LOGIN
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None).encode('utf8')
-    print(password)
-    
-    user = User.query.filter_by(email=email).first()
-    print(user)
-
-    if email != user.email or bcrypt.checkpw(password, user.password.encode('utf8')):
-        print(user.password)
-        return jsonify({"msg": "Bad username or password"}), 401
-        
-    access_token = create_access_token(identity=user.id)
-    print(access_token)
-    return jsonify({"user_id": user.id, "name": user.name, "token": access_token})
-
-#LOGOUT
-@app.route("/logout", methods=["DELETE"])
-@jwt_required()
-def logout():
-    token_unique_id = get_jwt()["jti"]
-    jwt_redis_blocklist.set(token_unique_id, "", ex=ACCESS_EXPIRES)
-    return jsonify(msg="Access token revoked")
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
