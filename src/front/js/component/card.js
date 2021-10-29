6abf81525bfd83bc2c1bc2f920913524bfc707c1
arantxamorcillo <arantxa.morcillo@gmail.com>
2021-10-29 20:18:31 +0000

    card & list stuled

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -4,7 +4,7 @@ import propTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { HashtagProfile } from "./hashtagProfile";
 import avatar1 from "../../img/avatar1.png";
-
+import { Swiper, SwiperSlide } from "swiper/react";
 export const Card = ({ result }) => {
 	const { store, actions } = useContext(Context);
 	const [buttonText, setButtonText] = useState("Conectar");
@@ -34,54 +34,37 @@ export const Card = ({ result }) => {
 	};
 	return (
 		<div className="card-result">
-			<div className="card-top-banner" />
-			<div className="card-body little-profile px-4 pt-0 pb-4">
+			<div className="card-body">
 				<div className="pro-img text-center">
-					<img src={result.profile_img ? result.profile_img : avatar1} alt="user" />
+					<img className="avatar-img" src={result.profile_img ? result.profile_img : avatar1} alt="user" />
 				</div>
-				<h3 className="text-center text-prin">{result.name ? result.name : ""}</h3>
-				<p className="text-start px-md-4 py-md-2 p-1">{result.description ? result.description : ""}</p>
+				<h2 className="text-center text-prin">{result.name ? result.name : ""}</h2>
+				<p className="text-start px-md-4 py-md-2 p-1">{`${result.age} años`}</p>
 
 				<div className="row justify-content-end">
 					<div className="col-12 d-flex flex-wrap justify-content-center">
-						{result.age ? <HashtagProfile icon="fas fa-history" text={`${result.age} años`} /> : ""}
-						{result.abortion_num ? (
-							<HashtagProfile
-								icon="fas fa-hand-holding-medical"
-								text={`${result.abortion_num} pérdida(s)`}
-							/>
-						) : (
-							""
-						)}
-						{result.center ? (
-							<HashtagProfile icon="fas fa-map-marker" text={`Centro ${result.center}`} />
-						) : (
-							""
-						)}
+						{result.abortion_num ? <HashtagProfile text={`${result.abortion_num} pérdida(s)`} /> : ""}
+						{result.center ? <HashtagProfile text={`Centro ${result.center}`} /> : ""}
 
-						{result.process ? (
-							<HashtagProfile icon="fas fa-search" text={`${result.process} año(s) en búsqueda`} />
-						) : (
-							""
-						)}
+						{result.process ? <HashtagProfile text={`${result.process} año(s) en búsqueda`} /> : ""}
 
-						{result.treatment ? (
-							<HashtagProfile icon="fas fa-briefcase-medical" text={result.treatment} />
-						) : (
-							""
-						)}
+						{result.treatment ? <HashtagProfile text={result.treatment} /> : ""}
 
-						{result.couple ? <HashtagProfile icon="fas fa-heart" text={result.couple} /> : ""}
+						{result.couple ? <HashtagProfile text={result.couple} /> : ""}
 					</div>
 				</div>
 			</div>
 
 			<div className="card-footer p-4">
 				<div className="row justify-content-center">
-					<div className="col-8 col-md-4 text-center">
-						<button onClick={matchUser}>{buttonText}</button>
+					<div className="col-12 col-md-6">
+						<button className="button primary " onClick={matchUser}>
+							{buttonText}
+						</button>
+					</div>
+					<div className="col-12 col-md-6">
 						<Link to={`/moreUserInfo/${result.id}`}>
-							<button>mas info</button>
+							<button className="button secondary">mas info</button>
 						</Link>
 					</div>
 				</div>
